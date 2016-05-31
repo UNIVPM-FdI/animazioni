75144baaa49d97e7d43c68e304b0a27dbbc8f4ab
BlackeWhite <gian.lu97@hotmail.it>
2016-05-31 11:22:59 +0200

    Add files via upload

---
+++ pallina.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main (){
+
+cout << "la pallina e' in quiete -->  O" << endl ;
+cout << "adesso non e' piu'' in qiuete    O" << endl;
+Sleep(600);
+cout << "                                  O" << endl;
+Sleep(600);
+cout << "                                    O" << endl;
+Sleep(600);
+cout << "                                     O" << endl;
+Sleep(600);
+cout << "                                      O" << endl;
+Sleep(600);
+cout << "                                       O" << endl;
+Sleep(600);
+cout << "                                        O" << endl;
+Sleep(600);
+cout << "                                         O" << endl;
+Sleep(600);
+cout << "                                           O" << endl;
+Sleep(600);
+cout << "           BOOM!!!                         __O___ " << endl;
+}
+
---
+++ personaggio.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main ()
+{
+	cout << "  O  " << endl;
+	cout << " /|\\ c " << endl;
+	cout << "  |    " << endl;
+	cout << " / \\    "    << endl;
+	cout << "--------" << endl;
+	Sleep(900);
+    cout << "        |   O  " << endl;
+	cout << "        |  /|\\ i  " << endl;
+	cout << "        |   |    " << endl;
+	cout << "        |  / \\    "    << endl;
+	cout << "         ------" << endl;
+		Sleep(900);
+    cout << "               |   O  " << endl;
+	cout << "               |  /|\\ a " << endl;
+	cout << "               |   |    " << endl;
+	cout << "               |  / \\    "    << endl;
+	cout << "               ------" << endl;
+	Sleep(900);
+    cout << "                     |   O  " << endl;
+	cout << "                     |  /|\\ o  " << endl;
+	cout << "                     |   |    " << endl;
+	cout << "                     |  / \\    "    << endl;
+	cout << "                     ------" << endl;
+}
