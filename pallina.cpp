c468d5b92655fd59780627b0caa5fc9cb8ea5b9a
BlackeWhite <gian.lu97@hotmail.it>
2016-05-31 11:23:27 +0200

    Update pallina.cpp

--- pallina.cpp
+++ pallina.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main (){
 
 cout << "la pallina e' in quiete -->  O" << endl ;
-cout << "adesso non e' piu'' in qiuete    O" << endl;
+cout << "adesso non e' piu' in qiuete    O" << endl;
 Sleep(600);
 cout << "                                  O" << endl;
 Sleep(600);
